842b18863ea7ce7201eaf363c8a3e57729618591
alexfont <alexfont321@gmail.com>
2018-08-23 13:20:24 -0500

    finished small project

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
+import LocationList from "./locationList/LocationList"
 
 class Kennel extends Component {
     render() {
         return (
             <div>
                 <h3>Dog Kennels</h3>
-                <h4>Nashville North Location</h4>
-                <h5>500 Puppy Way</h5>
-                <EmployeeList />
+                < LocationList />
+                < EmployeeList />
             </div>
         );
     }
